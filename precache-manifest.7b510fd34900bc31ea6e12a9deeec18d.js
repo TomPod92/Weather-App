ae4324cf3dff8d5eff2849f8837fb99f8bed1840
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2019-05-25 17:42:48 +0200

    Updates

---
+++ precache-manifest.7b510fd34900bc31ea6e12a9deeec18d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effceca600b79ffd6dd887844838b7c6",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "28a4b6d65729dc15c517",
+    "url": "/Weather-App/static/css/main.059a92d7.chunk.css"
+  },
+  {
+    "revision": "5853120c4201c2797d57",
+    "url": "/Weather-App/static/js/2.83cb3e56.chunk.js"
+  },
+  {
+    "revision": "28a4b6d65729dc15c517",
+    "url": "/Weather-App/static/js/main.1368fe01.chunk.js"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  }
+]);
